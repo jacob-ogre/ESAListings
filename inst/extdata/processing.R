be5e6dfa22271ebb15c4c54de79d38b75326943b
Jacob Malcom <jmalcom@defenders.org>
2017-01-14 18:05:47 -0500

    Update highcharter figures, lots of code formatting, add extdata.

--- inst/extdata/processing.R
+++ inst/extdata/processing.R
@@ -20,30 +20,31 @@ county_map <- readShapeSpatial("C:/Users/mevans/GIS/cb_2015_us_county_500k.shp")
 county_map@data <- left_join(county_map@data, counties, by = "GEOID")
 
 
-county_map@data$style<-pal(county_map@data$count)
+county_map@data$style <- pal(county_map@data$count)
 
 test_geo <- geojson_json(county_map)
 test_geo <- geojson_read("county_geo.json")
 
 #read topoJSON in list format
-test_topo <- jsonlite::fromJSON(readLines("county_topo3.json")%>%paste(collapse="\n"))
+test_topo <- jsonlite::fromJSON(readLines("county_topo3.json") %>%
+                                  paste(collapse="\n"))
 
 #read topoJSON in character vector format
-test_topo <- readLines("county_topo_rb76.json")%>%paste(collapse="\n")
+test_topo <- readLines("county_topo_rb76.json") %>% paste(collapse="\n")
 
 test_geo$features <- lapply(test_geo$features, function(x) {
   x$properties$style <- list(fillColor = palfx(x$properties$count))
   x})
 
-geojson_out <- jsonlite::toJSON(test_geo,pretty = TRUE)
-write(geojson_out,file="county_geo2.json")
+geojson_out <- jsonlite::toJSON(test_geo, pretty = TRUE)
+write(geojson_out, file="county_geo2.json")
 
 
-cur_map <- leaflet()%>%
-  addTopoJSON(topo_prac, weight = 0.75, color = "black", fillOpacity = 0.5)%>%
-  addProviderTiles("Stamen.TonerLite")%>%
-  setView(lng=-95, lat=38, zoom = 4) %>%
-  mapOptions(zoomToLimits = "never")%>%
+cur_map <- leaflet() %>%
+  addTopoJSON(topo_prac, weight = 0.75, color = "black", fillOpacity = 0.5) %>%
+  addProviderTiles("Stamen.TonerLite") %>%
+  setView(lng=-95, lat=38, zoom = 4)  %>%
+  mapOptions(zoomToLimits = "never") %>%
   addLegend(title = "Number of<br>Listed Species",
             position = "bottomleft",
             pal = palfx,
--- inst/listings-summary/server_area_page.R
+++ inst/listings-summary/server_area_page.R
@@ -11,18 +11,21 @@ server_area_page <- function(input, output, session) {
 
   #county map showing # of species per county
   output$map <- renderLeaflet({
-      leaflet()%>%
-      addTopoJSON(county_topo, weight = 0.5, color = "black", fillOpacity = 0.5)%>%
-      addProviderTiles("Stamen.TonerLite")%>%
-      setView(lng=-95, lat=38, zoom = 4) %>%
-      mapOptions(zoomToLimits = "never")%>%
+    leaflet() %>%
+      addTopoJSON(county_topo,
+                  weight = 0.5,
+                  color = "black",
+                  fillOpacity = 0.5) %>%
+      addProviderTiles("Stamen.TonerLite") %>%
+      setView(lng=-95, lat=38, zoom = 4)  %>%
+      mapOptions(zoomToLimits = "never") %>%
       addLegend(title = "Number of<br>Listed Species",
                 position = "bottomleft",
-                #pal = palfx,
-                #values = seq(0, 80, 10),
                 colors = palfx(seq(0, 80, 10)),
-                labels = c("- 0","- 10","- 20","- 30","- 40","- 50","- 60","- 70","> 80"))%>%
-      addLegend(title = "Click on the center of a county<br>to see the species listed there.", position = "topright", colors = NULL, labels = NULL)%>%
+                labels = c("- 0","- 10","- 20","- 30","- 40","- 50",
+                           "- 60","- 70","> 80")) %>%
+      addLegend(title = "Click on the center of a county<br>to see the species listed there.",
+                position = "topright", colors = NULL, labels = NULL) %>%
       addCircleMarkers(data = counties,
                       lng = ~INTPTLON,
                       lat = ~INTPTLAT,
@@ -36,21 +39,19 @@ server_area_page <- function(input, output, session) {
 
   observe({
     if(!is.null(input$specTble_cell_clicked$value)) {
-      #print(input$specTble_cell_clicked$value)
     spec_select <- filter(esacounties,
-                          Scientific == input$specTble_cell_clicked$value)%>%
+                          Scientific == input$specTble_cell_clicked$value) %>%
                    select(GEOID)
-    # print(spec_select$GEOID)
-    leafletProxy("map")%>%
-    clearShapes()%>%
-    addCircles(data = filter(counties,GEOID %in% spec_select$GEOID),
-                   lng = ~INTPTLON,
-                   lat = ~INTPTLAT,
-                   radius = 7000,
-                   color = "black",
-                   fillOpacity = 1,
-                   stroke = FALSE
-    )
+
+    leafletProxy("map") %>%
+      clearShapes() %>%
+      addCircles(data = filter(counties,GEOID %in% spec_select$GEOID),
+                     lng = ~INTPTLON,
+                     lat = ~INTPTLAT,
+                     radius = 7000,
+                     color = "black",
+                     fillOpacity = 1,
+                     stroke = FALSE)
     }
   })
 
@@ -59,21 +60,21 @@ server_area_page <- function(input, output, session) {
     click_lon <- input$map_marker_click[[4]]
     gid <- counties$GEOID[counties$INTPTLAT == click_lat & counties$INTPTLON == click_lon]
     output$specTble <- DT::renderDataTable({
-      filter(esacounties, GEOID == gid)%>%
-      select(Scientific, Common)%>%
+      filter(esacounties, GEOID == gid) %>%
+      select(Scientific, Common) %>%
       datatable(rownames = FALSE, selection = "single", colnames = c("Species", "Common Name"), caption = "Click a species name to see it's range displayed")
-      })
+    })
   })
 
   output$sp_cumm <- renderPlotly({
     #cummulative number of counties per species
-    py<-plot_ly(arrange(species, count), x = ~count, source = "sp_dist")%>%
+    py<-plot_ly(arrange(species, count), x = ~count, source = "sp_dist") %>%
       add_lines(y = ~round(row_number(count)/nrow(species), 3), name = "Cumulative",
             type = "scatter", mode = "lines",
-            text = ~paste(round(row_number(count)/nrow(species),3)*100,"% of species occur in", count, "or fewer counties"), hoverinfo = "text")%>%
+            text = ~paste(round(row_number(count)/nrow(species),3)*100,"% of species occur in", count, "or fewer counties"), hoverinfo = "text") %>%
       add_histogram(histnorm = "probability", name = "Histogram", xbins = list(start = 0.5, end = 3102.5, size = 1),
                     text = ~paste("% of counties contain", "listed species"),
-                    hoverinfo = "none")%>%
+                    hoverinfo = "none") %>%
       layout(title = "Listed Species Range Sizes<br>(click to see species)",
              xaxis = list(title = "Number of Counties in Species' Range", type = "log", tickvals = c(0, 1, 5, 10, 50, 100, 500, 1000, 3000)),
              yaxis = list(title = "Percentile of Species"),
@@ -82,14 +83,14 @@ server_area_page <- function(input, output, session) {
 
   output$cn_cumm <- renderPlotly({
   #cummulative number of counties per species
-    plot_ly(arrange(counties, count), x = ~count)%>%
+    plot_ly(arrange(counties, count), x = ~count) %>%
       add_lines(y = ~round(row_number(count)/nrow(counties), 3), name = "Cumulative",
             type = "scatter", mode = "lines",
             text = ~paste(round(row_number(count)/nrow(counties),3)*100,"% of counties contain", count, "or fewer species"),
-            hoverinfo = "text")%>%
+            hoverinfo = "text") %>%
       add_histogram(histnorm = "probability", name = "Histogram",
                   #text = ~paste("% of counties contain", "listed species"),
-                    hoverinfo = "none")%>%
+                    hoverinfo = "none") %>%
       layout(title = "Listed Species per U.S. County",
              xaxis = list(title = "Number of Listed Species per County", type = "log", tickvals = c(0, 1, 5, 10, 50, 100, 1000)),
              yaxis = list(title = "Percentile of Counties"),
@@ -97,14 +98,18 @@ server_area_page <- function(input, output, session) {
 
   })
 
- observeEvent(event_data("plotly_click", source = "sp_dist"),{
-  print(event_data("plotly_click", source = "sp_dist"))
-  sp_count <- event_data("plotly_click", source = "sp_dist")$x[[1]]
-  output$rngTble <- DT::renderDataTable({
-   filter(ungroup(species), count == sp_count)%>%
-   inner_join(TECP_domestic, by = c("Scientific" = "Scientific_Name"))%>%
-   select(Scientific, Common_Name, count)%>%
-   datatable(rownames = FALSE, selection = "none", colnames = c("Species", "Common Name", "Counties"))
-})})
+  observeEvent(event_data("plotly_click", source = "sp_dist"), {
+    print(event_data("plotly_click", source = "sp_dist"))
+    sp_count <- event_data("plotly_click", source = "sp_dist")$x[[1]]
+    output$rngTble <- DT::renderDataTable({
+      filter(ungroup(species), count == sp_count) %>%
+        inner_join(TECP_domestic, by = c("Scientific" = "Scientific_Name")) %>%
+        select(Scientific, Common_Name, count) %>%
+        datatable(rownames = FALSE,
+                  selection = "none",
+                  colnames = c("Species", "Common Name", "Counties"))
+    })
+  })
+
 }
 
